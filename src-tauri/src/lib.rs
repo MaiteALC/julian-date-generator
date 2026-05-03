a98ddecabf5ba8cafcee0432d0307aa0cedc3cce
MaiteALC <maitexp549@gmail.com>
2026-05-03 14:27:15 -0300

    refactor(backend): alter parameter types
    - Change the 'revert_julian_date()' function parameters from 'i32' and 'u32' to 'u16', limiting the input range and blocking negative numbers.
    - Add casting to satisfy the type requirements of 'NaiveDate::from_yo_opt()'.

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -24,10 +24,13 @@ fn calculate_date(full_year: bool, separator: String, reverse_order: bool, leadi
 }
 
 #[tauri::command]
-fn revert_julian_date(year: i32, julian_day: u32) -> Result<String, String> {
+fn revert_julian_date(year: u16, julian_day: u16) -> Result<String, String> {
     use chrono::NaiveDate;
 
-    let data: NaiveDate = NaiveDate::from_yo_opt(year, julian_day)
+    let chrono_year: i32 = year as i32;
+    let chrono_julian_day: u32 = julian_day as u32;
+
+    let data: NaiveDate = NaiveDate::from_yo_opt(chrono_year, chrono_julian_day)
         .ok_or("Data juliana inválida")?;
     
     Ok(data.format("%d/%m/%Y").to_string())
